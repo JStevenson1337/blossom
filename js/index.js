5301e66808b9ba58516dc5e2d9a560af68808be2
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-04-18 18:55:26 -0500

    add: "getLocation" function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+  getLocation();
+};
+
+
+var x = document.getElementById("location");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
 const mymap = L.map('map').setView([0, 0], 1);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -9,49 +28,3 @@ tiles.addTo(mymap);
 getMap();
 setInterval(getMap, 1000);
 
-$(document).ready(function () {
-  var params = OSM.params();
-
-  if (params.lat && params.lon) {
-    params.lat = parseFloat(params.lat);
-    params.lon = parseFloat(params.lon);
-    params.zoom = params.zoom || 17;
-
-    var url = "/edit";
-
-    if (params.editor) {
-      url += "?editor=" + params.editor;
-    }
-
-    url += OSM.formatHash(params);
-
-    $(".start-mapping").attr("href", url);
-  } else {
-    var geoSuccess = function (position) {
-      window.location = "/edit" + OSM.formatHash({
-        zoom: 17,
-        lat: position.coords.latitude,
-        lon: position.coords.longitude
-      });
-    };
-
-    $(".start-mapping").on("click", function (e) {
-      e.preventDefault();
-      $(".start-mapping").addClass("loading");
-
-      if (navigator.geolocation) {
-        // handle firefox's weird implementation
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=675533
-        window.setTimeout(manualEdit, 4000);
-
-        navigator.geolocation.getCurrentPosition(geoSuccess, manualEdit);
-      } else {
-        manualEdit();
-      }
-    });
-  }
-
-  function manualEdit() {
-    window.location = "/?edit_help=1";
-  }
-});
