0d7b359f852a7e55d7a6f40376a6bc967902eb48
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-04-19 08:33:31 -0500

    Add: git ignore Fix: iter over null byte

--- index.py
+++ index.py
@@ -6,8 +6,8 @@ import requests, lxml, re, json
 from bs4 import BeautifulSoup, ResultSet
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import pandas as pd
-from IPython.display import Image, HTML
+# import pandas as pd
+# from IPython.display import Image, HTML
 
 
 headers = {
@@ -16,18 +16,20 @@ headers = {
 }
 
 # https://serpapi.com/playground?q=Flowers&tbm=isch&ijn=0
-params = {
-    "q": "Flowers",
-    "tbm": "isch",
-    "ijn": "0",
-}
+# params = {
+#     "q": "Flowers",
+#     "tbm": "isch",
+#     "ijn": "0",
+# }
 
-html = requests.get("https://unsplash.com/s/photos/flowers")
+
+# html = requests.get("https://unsplash.com/s/photos/flowers")
+html = requests.get("https://www.google.com/search?q=flowers&tbm=isch&source=hp&biw=948&bih=952&ei=bKteYufDIoS4qtsP_IG5oAo&iflsig=AHkkrS4AAAAAYl65fP8neUwk2mlQr7kj0Lhw_6TUBNdE&ved=0ahUKEwjnqeXZkKD3AhUEnGoFHfxADqQQ4dUDCAc&uact=5&oq=flowers&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMgUIABCABDIICAAQgAQQsQMyCAgAEIAEELEDMgsIABCABBCxAxCDATIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQNQAFjkCWDtCmgAcAB4AIABhgGIAZYEkgEDNS4ymAEAoAEBqgELZ3dzLXdpei1pbWc&sclient=img")
 soup = BeautifulSoup(html.text, "lxml")
 image_container = soup.find_all('img')
 example = image_container[0]
 example.attrs['src']
-FILE = open("images.txt", "w")
+FILE = open("images.txt", "a")
 try:
     for image in image_container:
         FILE.write(image.attrs['src'] + "\n")
@@ -42,9 +44,9 @@ with open("images.txt", "r") as f:
         print(line)
         try:
             response = requests.get(line, stream=True)
-            while line:
-                line = response.raw.read(1024)
-                with open(line, "wb") as f:
+            for chunk in response.iter_content(chunk_size=128):
+                with open("images/image.jpg", "wb") as f:
+                    f.write(chunk)
                     shutil.copyfileobj(response.raw, f)
                     image = mpimg.imread(line)
                     imgplot = plt.imshow(image)
